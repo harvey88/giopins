36afadc88e2aa5107e594ec455ffb32db17544f8
illia <badorillia@gmail.com>
2019-04-15 17:09:56 +0300

    auth, create pin, mutations, query

---
+++ resolvers.js
@@ -0,0 +1,38 @@
+const { AuthenticationError } = require('apollo-server')
+const Pin = require('./models/Pin')
+
+// const user = {
+//     _id: "1",
+//     name: "Reed",
+//     email: "reedbarger@gmail.com",
+//     picture: "https://cloudinary.com/sdlgh"
+// }
+
+const authenticated = next => (root, args, ctx, info) => {
+    if(!ctx.currentUser) {
+        throw new AuthenticationError('You must be logged in')
+    }
+    return next(root, args, ctx, info)
+}
+
+module.exports = {
+    Query: {
+        me: authenticated((root, args, ctx) => ctx.currentUser ),
+        getPins: async (root, args, ctx) => {
+            const pins = await Pin.find({})
+                            .populate('author')
+                            .populate('comments.author')
+            return pins 
+        } 
+    },
+    Mutation: {
+        createPin: authenticated(async (root, args, ctx) => {
+            const newPin = await new Pin({
+                ...args.input,
+                author: ctx.currentUser._id
+            }).save()
+            const pinAdded = await Pin.populate(newPin, 'author')
+            return pinAdded
+        })
+    }
+}
