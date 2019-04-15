e11676511087390f521bbd94b925b0fc18f123b1
illia <badorillia@gmail.com>
2019-04-15 14:53:03 +0300

    auth, create pin, load pins

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,11 +1,126 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
+import ReactMapGL, { NavigationControl, Marker, Popup } from "react-map-gl"
 import { withStyles } from "@material-ui/core/styles";
+import differenceInMinutes from 'date-fns/difference_in_minutes'
 // import Button from "@material-ui/core/Button";
 // import Typography from "@material-ui/core/Typography";
 // import DeleteIcon from "@material-ui/icons/DeleteTwoTone";
 
+import { useClient } from '../client'
+import { GET_PINS_QUERY } from '../graphql/queries'
+import PinIcon from "./PinIcon"
+import Blog from "./Blog"
+import Context from "../context"
+
+const INITIAL_VIEWPORT = {
+  latitude: 50.45466,
+  longitude: 30.5238,
+  zoom: 15
+}
+
 const Map = ({ classes }) => {
-  return <div>Map</div>;
+  const client = useClient()
+  const { state, dispatch } = useContext(Context)
+  useEffect(() => {
+    getPins()
+  }, [])
+  const [ viewport, setViewport ] = useState(INITIAL_VIEWPORT)
+  const [ userPosition, setUserPosition ] = useState(null)
+
+  useEffect(() => {
+    getUserPosition()
+  }, [])
+
+
+  const getUserPosition = () => {
+    if('geolocation' in navigator) {
+      navigator.geolocation.getCurrentPosition(position => {
+        const { latitude, longitude } = position.coords
+        setViewport({ ...viewport, latitude, longitude })
+        setUserPosition({ latitude, longitude })
+      })
+    }
+  }
+
+  const getPins = async () => {
+    const { getPins } = await client.request(GET_PINS_QUERY)
+    dispatch({ type: "GET_PINS", payload: getPins })
+  }
+
+  const handleMapClick = ({ lngLat, leftButton }) => {
+    if(!leftButton) return
+    if(!state.draft) {
+      dispatch({ type: "CREATE_DRAFT" })
+    }
+
+    const [ longitude, latitude ] = lngLat
+    dispatch({
+      type: "UPDATE_DRAFT_LOCATION",
+      payload: { longitude, latitude }
+    })
+  }
+
+  const highlightNewPin = pin => {
+    const isNewPin = differenceInMinutes(Date.now(), Number(pin.createdAt)) <= 30
+    return isNewPin ? "limegreen" : "darkblue"
+  }
+
+  return (
+    <div className={classes.root}>
+      <ReactMapGL
+        width="100vw"
+        height="calc(100vh - 64px)"
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+        mapboxApiAccessToken="pk.eyJ1IjoiZWxpamFoNDQiLCJhIjoiY2p1Y3IwOWEyMGV1bDN5cDliYzc1MHVqaSJ9.pYeMfW-DRFvpmDZcgB2Atw"
+        onViewportChange={newViewport => setViewport(newViewport)}
+        onClick={handleMapClick}
+        {...viewport}
+      >
+        <div className={classes.navigationControl}> 
+          <NavigationControl 
+            onViewportChange={newViewport =>  setViewport(newViewport)}
+          />
+        </div>
+
+        {userPosition && (
+          <Marker
+            latitude={userPosition.latitude}
+            longitude={userPosition.longitude}
+            offsetLeft={-19}
+            offsetTop={-37}
+          >
+            <PinIcon size={40} color="red" />
+          </Marker>
+        )}
+
+        {state.draft && (
+          <Marker
+            latitude={state.draft.latitude}
+            longitude={state.draft.longitude}
+            offsetLeft={-19}
+            offsetTop={-37}
+          >
+            <PinIcon size={40} color="hotpink" />
+          </Marker>
+        )}
+
+        {state.pins.map(pin => (
+          <Marker
+            key={pin._id}
+            latitude={pin.latitude}
+            longitude={pin.longitude}
+            offsetLeft={-19}
+            offsetTop={-37}
+          >
+            <PinIcon size={40} color={highlightNewPin(pin)} />
+          </Marker>
+        ))}
+
+      </ReactMapGL>
+
+      <Blog />
+    </div>
+    )
 };
 
 const styles = {
